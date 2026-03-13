e261dc4ebd717f9d32b4784357e4e4e496e64840
esmarr58 <ruben.estrada@hetpro.com.mx>
2026-03-12 19:03:13 -0600

    Update hex7seg.v

--- hex7seg.v
+++ hex7seg.v
@@ -5,6 +5,7 @@ module hex7seg(
 
 always @(*) begin
     case(bin)
+
         4'h0: seg = 7'b1000000;
         4'h1: seg = 7'b1111001;
         4'h2: seg = 7'b0100100;
@@ -15,13 +16,16 @@ always @(*) begin
         4'h7: seg = 7'b1111000;
         4'h8: seg = 7'b0000000;
         4'h9: seg = 7'b0010000;
+
         4'hA: seg = 7'b0001000;
         4'hB: seg = 7'b0000011;
         4'hC: seg = 7'b1000110;
         4'hD: seg = 7'b0100001;
         4'hE: seg = 7'b0000110;
         4'hF: seg = 7'b0001110;
+
         default: seg = 7'b1111111;
+
     endcase
 end
 

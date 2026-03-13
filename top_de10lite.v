3e30f1b634980e12d1323fe822169752b4f28a3b
esmarr58 <ruben.estrada@hetpro.com.mx>
2026-03-12 18:43:48 -0600

    Update top_de10lite.v

--- top_de10lite.v
+++ top_de10lite.v
@@ -8,13 +8,21 @@ module top_de10lite (
     wire [3:0] cpu_out;
     wire       cpu_halt;
     wire       rst;
+    wire       slow_clk;
 
-    // En la DE10-Lite los botones KEY suelen ser activos en bajo
+    // KEY[0] activo en bajo
     assign rst = ~KEY[0];
+
+    // Divisor de reloj para ver el funcionamiento en LEDs
+    clock_divider div0 (
+        .clk_in(CLOCK_50),
+        .rst(rst),
+        .clk_out(slow_clk)
+    );
 
     // Instancia del procesador
     cpu4 cpu0 (
-        .clk(CLOCK_50),
+        .clk(slow_clk),
         .rst(rst),
         .in_data(SW[3:0]),
         .out_data(cpu_out),
@@ -22,8 +30,8 @@ module top_de10lite (
     );
 
     // Salidas a LEDs
-    assign LEDR[3:0] = cpu_out;     // salida del procesador
-    assign LEDR[4]   = cpu_halt;    // indicador de halt
+    assign LEDR[3:0] = cpu_out;
+    assign LEDR[4]   = cpu_halt;
     assign LEDR[9:5] = 5'b00000;
 
 endmodule

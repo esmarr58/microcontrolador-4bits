66c72f115242e8fd8b3f2b1f6b3b372c295b6738
esmarr58 <ruben.estrada@hetpro.com.mx>
2026-03-12 18:10:15 -0600

    Create ir.v

---
+++ ir.v
@@ -0,0 +1,16 @@
+module ir (
+    input clk,
+    input rst,
+    input load,
+    input [7:0] d,
+    output reg [7:0] q
+);
+
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        q <= 8'b0000_0000;
+    else if (load)
+        q <= d;
+end
+
+endmodule

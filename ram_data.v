0817d38c267f5c849d8f8cdb1212d54248c1f5dc
esmarr58 <ruben.estrada@hetpro.com.mx>
2026-03-12 18:39:04 -0600

    Update ram_data.v

--- ram_data.v
+++ ram_data.v
@@ -1,18 +1,18 @@
 module ram_data (
     input clk,
-    input we,              // write enable
-    input [3:0] addr,      // dirección
-    input [3:0] din,       // dato de entrada
-    output reg [3:0] dout  // dato de salida
+    input we,
+    input [3:0] addr,
+    input [3:0] din,
+    output [3:0] dout
 );
 
-    // memoria de 16 palabras de 4 bits
     reg [3:0] mem [15:0];
 
     always @(posedge clk) begin
         if (we)
-            mem[addr] <= din;   // escritura
-        dout <= mem[addr];      // lectura
+            mem[addr] <= din;
     end
+
+    assign dout = mem[addr];
 
 endmodule
